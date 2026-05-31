c08d92b537aa23f1caf64a4dea0424c723e053ed
louisjacquier <louis.marie.jacquier@gmail.com>
2026-05-31 16:47:08 +0200

    ajout composant Logo Krypto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const navigate = useNavigate()
 
   async function handleLogin() {
-    const response = await fetch('http://localhost:5000/auth/login', {
+    const response = await fetch('https://meteo-server-bz30.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
